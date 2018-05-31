929c3fc83f071e495aaf152d02fb59f6e421eee5
Yuki <18482177461@163.com>
2018-05-31 18:13:53 +0800

    src文件下的资源才可访问

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-// import logo1 from './../material/logo1.png'
+import logo1 from './material/logo1.png'
 import './App.css';
 
 class App extends Component {
@@ -8,9 +8,10 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={logo1} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
+        {/* <div className=""></div> */}
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
